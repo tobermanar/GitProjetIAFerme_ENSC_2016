61d0462bdc83d41b261125eb1611a6ec7fa05178
Spectantibus <antoine.letreguilly@free.fr>
2016-04-15 07:27:13 +0100

    Q4 fully functionnal

--- IA_ARMAND_BERNARD_LETREGUILLY/Form1.cs
+++ IA_ARMAND_BERNARD_LETREGUILLY/Form1.cs
@@ -232,11 +232,11 @@ namespace IA_ARMAND_BERNARD_LETREGUILLY
             }
             if (nombreLettresValides == 0)
             {
-                //TODO Ajouter appel a fonction
+                string[] result = NodeChemin.TrouveChemin(lettresDecoupes, true);
                 listBox2.BeginUpdate();
                 listBox2.Items.Add("  Question 4 : ");
-                // listBox2.Items.Add("Distance entre " + param1 + " et " + param2 + " = " + result[0]);
-                // listBox2.Items.Add("Chemin : " + result[1]);
+                listBox2.Items.Add("Distance du trajet total : " + result[0]);
+                listBox2.Items.Add("Chemin : " + result[1]);
                 listBox2.Items.Add("Fin question 4");
                 listBox2.EndUpdate();
             }
